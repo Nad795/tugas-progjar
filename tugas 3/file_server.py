43891b555e46a9070507764bdca1ebf432595976
Nad795 <nadineirawan2005@gmail.com>
2025-05-23 21:49:36 +0700

    tugas 3

--- tugas 3/file_server.py
+++ tugas 3/file_server.py
@@ -18,7 +18,7 @@ class ProcessTheClient(threading.Thread):
 
     def run(self):
         while True:
-            data = self.connection.recv(1024)
+            data = self.connection.recv(1048576)
             if data:
                 d = data.decode()
                 hasil = fp.proses_string(d)
