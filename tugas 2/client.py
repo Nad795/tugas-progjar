52fd8d399bf9adee0ed660eae739738d443ca34e
Nad795 <nadineirawan2005@gmail.com>
2025-05-23 08:18:45 +0700

    tugas 2

---
+++ tugas 2/client.py
@@ -0,0 +1,20 @@
+import socket
+
+server_address = ('172.20.152.241', 45000)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(server_address)
+
+try:
+    while True:
+        message = input()
+        
+        request = message + "\r\n"
+        sock.sendall(message.encode('utf-8'))
+        data = sock.recv(1024)
+        
+        response = data.decode('utf-8')
+        print(response.strip())
+        if response.strip() == "invalid request":
+            break
+finally:
+    sock.close()
---
+++ tugas 2/server.py
@@ -0,0 +1,55 @@
+from socket import *
+import socket
+import threading
+import logging
+import datetime
+import sys
+
+class ProcessTheClient(threading.Thread):
+    def __init__(self,connection,address):
+        self.connection = connection
+        self.address = address
+        threading.Thread.__init__(self)
+     
+    def run(self):
+        while True:
+            data = self.connection.recv(32)
+            if data:
+                data = data.decode('utf-8')
+                if data.startswith("TIME") and data.endswith("\r\n"):
+                    response = "JAM " + datetime.strftime(datetime.now(), "%H:%M:%S") + "\r\n"
+                    print(f"Sending response to client {self.address}")
+                    self.connection.sendall(response.encode('utf-8'))
+                elif data.startswith("QUIT"):
+                    print(f"Client from {self.address} has exited")
+                    response = "invalid request \r\n"
+                    self.connection.sendall(response.encode('utf-8'))
+                else:
+                    print(f"Invalid request from client {self.address}")
+                    self.connection.close()
+                    break
+
+class Server(threading.Thread):
+	def __init__(self):
+		self.the_clients = []
+		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		threading   .Thread.__init__(self)
+
+	def run(self):
+		self.my_socket.bind(('0.0.0.0',45000))
+		self.my_socket.listen(1)
+		while True:
+			self.connection, self.client_address = self.my_socket.accept()
+			logging.warning(f"connection from {self.client_address}")
+			
+			clt = ProcessTheClient(self.connection, self.client_address)
+			clt.start()
+			self.the_clients.append(clt)
+	
+
+def main():
+	svr = Server()
+	svr.start()
+
+if __name__=="__main__":
+	main()
